0a293b7ba77cdc06a04c3e17c4dc08d6fdff40d7
unish <unishmhrjan@gmail.com>
2026-08-09 09:51:33 +0545

    checkpoint

--- mavenproject1-web/src/main/java/com/mycompany/PurchaseBean.java
+++ mavenproject1-web/src/main/java/com/mycompany/PurchaseBean.java
@@ -1,25 +1,46 @@
 package com.mycompany;
 
+import java.util.ArrayList;
 import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import purchase.PurchaseDao;
 import purchase.PurchaseEntity;
 
 @ManagedBean(name = "purchaseBean")
-@ViewScoped
+@SessionScoped
 public class PurchaseBean {
 
     @EJB
     private PurchaseDao dao;
 
     private PurchaseEntity entity = new PurchaseEntity();
+    List<PurchaseEntity> items = new ArrayList<>();
+
+    @PostConstruct
+    public void init() {
+        items.add(new PurchaseEntity()); // first row
+    }
+
+    public void addRow() {
+        items.add(new PurchaseEntity());
+    }
+
+    public void removeRow(PurchaseEntity item) {
+        items.remove(item);
+    }
+
+    public List<PurchaseEntity> getItems() {
+        return items;
+    }
 
     public void save() {
-            try {
+        try {
             dao.save(entity);
             FacesContext.getCurrentInstance().addMessage(null,
                     new FacesMessage(
@@ -43,6 +64,9 @@ public class PurchaseBean {
     }
 
     //getter and setter
+    
+    
+    
     public PurchaseDao getDao() {
         return dao;
     }
